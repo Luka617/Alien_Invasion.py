ffc49980af5562516243902625dfde4014cbb93b
Luka617 <78433146+Luka617@users.noreply.github.com>
2021-05-10 16:14:12 -0300

    Add files via upload

---
+++ alien_invasion/settings.py
@@ -0,0 +1,27 @@
+class Settings():
+    """Uma classe para armazenar todas as configurções do jogo."""
+
+    def __init__(self):
+        """Inicializa as configurações do jogo."""
+        #Configurações de tela
+        self.screen_width = 1000
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
+
+        #Configurações da espaçonave
+        self.ship_speed_factor = 1.5
+        self.ship_limit = 3
+
+        #Configurações dos projéteis
+        self.bullet_speed_factor = 3
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
+
+        # Configurações dos alienígenas
+        self.alien_speed_factor = 1
+
+        self.fleet_drop_speed = 10
+        # fleet_direction igual a 1 representa a direita; -1 representa a esquerda
+        self.fleet_direction = 1
